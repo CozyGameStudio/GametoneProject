3abd247492576d45f522b788d1825704b57f3daf
ovthemoon <seungmoonchoi71@gmail.com>
2024-01-20 15:57:29 +0900

    테스팅용 버튼 및 fps 체크 텍스트 추가
    프로파일러 탐구 겸 원활한 테스팅을 위한 기능 추가

---
+++ Assets/2.Scripts/FpsChecker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class FpsChecker : MonoBehaviour
+{
+    public TMP_Text fpsText;
+    float deltaTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+    }
+}
