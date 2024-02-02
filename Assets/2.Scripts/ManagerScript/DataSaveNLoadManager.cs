dafddaee18914d85a0249ce5fceb82c3c81061fd
ovthemoon <seungmoonchoi71@gmail.com>
2024-02-02 21:48:25 +0900

    Error Fix
    DataSaveNLoad nameSpace 누락 수정

--- Assets/2.Scripts/ManagerScript/DataSaveNLoadManager.cs
+++ Assets/2.Scripts/ManagerScript/DataSaveNLoadManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using UnityEngine.SceneManagement;
 //using UnityEngine.SceneManagement;
 [Serializable]
 public class BusinessData
