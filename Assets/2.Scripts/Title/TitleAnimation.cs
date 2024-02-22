acb5ac0afe88474954f8587e3eb61585f6ccb6e5
ovthemoon <seungmoonchoi71@gmail.com>
2024-02-22 22:48:13 +0900

    스테이지 세팅 / 스크립터블 오브젝트 수정 / 캔버스 프리팹 수정
    스테이지 세팅 오류 수정
    스크립터블 오브젝트 재생성
    캔버스 프리팹 수정

--- Assets/2.Scripts/Title/TitleAnimation.cs
+++ Assets/2.Scripts/Title/TitleAnimation.cs
@@ -10,11 +10,13 @@ public class TitleAnimation : MonoBehaviour
     public Image logo;
     public Image backImage;
     public Image startButton;
+    public Image titleImage;
     Sequence titleSequnce;
     void Start(){
         logo.color = new Color(logo.color.r, logo.color.g, logo.color.b, 0f);
         backImage.color = new Color(backImage.color.r, backImage.color.g, backImage.color.b, 0f);
         startButton.color = new Color(startButton.color.r, startButton.color.g, startButton.color.b, 0f);
+        titleImage.color = new Color(titleImage.color.r, titleImage.color.g, titleImage.color.b, 0f);
         startButton.GetComponent<Button>().interactable=false;
         StartCoroutine(TitleAnim());
     }
@@ -24,6 +26,7 @@ public class TitleAnimation : MonoBehaviour
         titleSequnce = DOTween.Sequence()
                 .Append(logo.DOFade(0, 2))
                 .Append(backImage.DOFade(1, 2))
+                .Append(titleImage.DOFade(1, 1))
                 .Append(startButton.DOFade(1, 2))
                 .OnComplete(() => startButton.GetComponent<Button>().interactable = true);
     }
